e4d06055d528770f53cde90b5fb9c988c07f2158
seobinpark <s2019w28@e-mirim.hs.kr>
2020-08-23 08:02:11 +0900

    #2 스플래시 화면 제작 완료

---
+++ app/src/main/java/com/example/NUNU/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.NUNU;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(2000); //대기 2초로 설정
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
