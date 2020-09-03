eb5cc277a8e511fb8f0b5c9e7f732befef5abf31
seobinpark <s2019w28@e-mirim.hs.kr>
2020-09-03 21:07:56 +0900

    #3 페이지 하단 부분 (그래프 xml 제작)

---
+++ app/src/main/java/com/example/NUNU/Greeting.java
@@ -0,0 +1,26 @@
+package com.example.NUNU;
+
+public class Greeting {
+    String username;
+    String greeting;
+
+    public Greeting(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+}
--- app/src/main/java/com/example/NUNU/User.java
+++ app/src/main/java/com/example/NUNU/User.java
@@ -3,112 +3,62 @@ package com.example.NUNU;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import android.graphics.Color;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import java.util.ArrayList;
-import java.util.List;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link User#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class User extends Fragment {
-    View view;
-    private LineChart lineChart;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public User() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment User_page.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static User newInstance(String param1, String param2) {
+        User fragment = new User();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.fragment_user, container, false);
-
-
-        lineChart = (LineChart) view.findViewById(R.id.chart);
-
-        List<Entry> entry1 = new ArrayList<>();
-        entry1.add(new Entry(1, 1));
-        entry1.add(new Entry(2, 2));
-        entry1.add(new Entry(3, 0));
-        entry1.add(new Entry(4, 4));
-        entry1.add(new Entry(5, 3));
-
-        List<Entry> entry2 = new ArrayList<>();
-        entry2.add(new Entry(1, 5));
-        entry2.add(new Entry(2, 3));
-        entry2.add(new Entry(3, 1));
-        entry2.add(new Entry(4, -1));
-        entry2.add(new Entry(5, -3));
-
-
-        LineDataSet set1 = new LineDataSet(entry1, "왼쪽 시력");
-        LineDataSet set2 = new LineDataSet(entry2, "오른쪽 시력");
-
-        set1.setLineWidth(2);
-        set1.setCircleRadius(6);
-        set1.setCircleColor(Color.parseColor("#FFA1B4DC"));
-        //lineDataSet.setCircleColorHole(Color.BLUE);
-        set1.setColor(Color.parseColor("#FFA1B4DC"));
-        set1.setDrawCircleHole(true);
-        set1.setDrawCircles(true);
-        set1.setDrawHorizontalHighlightIndicator(false);
-        set1.setDrawHighlightIndicators(false);
-        set1.setDrawValues(false);
-
-        set2.setLineWidth(2);
-        set2.setCircleRadius(6);
-        set2.setCircleColor(Color.parseColor("#646EFF"));
-        //lineDataSet.setCircleColorHole(Color.BLUE);
-        set2.setColor(Color.parseColor("#646EFF"));
-        set2.setDrawCircleHole(true);
-        set2.setDrawCircles(true);
-        set2.setDrawHorizontalHighlightIndicator(false);
-        set2.setDrawHighlightIndicators(false);
-        set2.setDrawValues(false);
-
-        LineData chartData = new LineData();
-        chartData.addDataSet(set1);
-
-        chartData.addDataSet(set2);
-        lineChart.setData(chartData);
-
-        XAxis xAxis = lineChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setTextColor(Color.BLACK);
-        xAxis.enableGridDashedLine(8, 24, 0);
-
-        YAxis yLAxis = lineChart.getAxisLeft();
-        yLAxis.setTextColor(Color.BLACK);
-
-        YAxis yRAxis = lineChart.getAxisRight();
-        yRAxis.setDrawLabels(false);
-        yRAxis.setDrawAxisLine(false);
-        yRAxis.setDrawGridLines(false);
-
-        Description description = new Description();
-        description.setText("");
-
-        lineChart.setDoubleTapToZoomEnabled(false);
-        lineChart.setDrawGridBackground(false);
-        lineChart.setDescription(description);
-        // lineChart.animateY(2000, Easing.EasingOption.EaseInCubic);
-        lineChart.invalidate();
-       // return inflater.inflate(R.layout.fragment_user, container, false);
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false);
     }
-
-
 }
