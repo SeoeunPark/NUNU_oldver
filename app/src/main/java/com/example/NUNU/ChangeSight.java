8ce4c20ec99d0fc6f201fe5fb1cbdea838ca4835
seobin Park <seobin0203@naver.com>
2020-09-30 21:04:09 +0900

    #5 사용자가 시력 입력을 잘못하면 토스트로 틀린 요소를 상세히 나타내도록 구현

--- app/src/main/java/com/example/NUNU/ChangeSight.java
+++ app/src/main/java/com/example/NUNU/ChangeSight.java
@@ -69,6 +69,7 @@ public class ChangeSight extends Fragment {
         ImageButton gobackbtn = (ImageButton)rootView.findViewById(R.id.exit2);
         SimpleDateFormat fdate = new SimpleDateFormat("MM-dd");
         Date date = new Date();
+
         gobackbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -76,6 +77,8 @@ public class ChangeSight extends Fragment {
             }
         });
         ImageButton donebtn = (ImageButton)rootView.findViewById(R.id.done2);
+        final Boolean[] check = {true};
+        final Boolean[] check2 = {true};
         donebtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -83,13 +86,45 @@ public class ChangeSight extends Fragment {
                     Toast.makeText(context,"변경할 좌안 시력을 입력해주세요.",Toast.LENGTH_SHORT).show();
                 }else if(TextUtils.isEmpty(editright.getText().toString())){
                     Toast.makeText(context,"변경할 우안 시력을 입력해주세요.",Toast.LENGTH_SHORT).show();
+                }else if(check[0]){
+                    char tmp;
+                    int count = 0;
+                    for(int i=0; i<editleft.getText().toString().length();i++) {
+                        tmp = editleft.getText().toString().charAt(i);
+                        int num = (int)tmp;
+                        if(!(num==45 || num==46 || (num>=48 && num<=57))) {
+                            count++;
+                        }
+                    }
+                    if(count!=0){
+                        Toast.makeText(context, "좌안 시력을 숫자로 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        check[0] =true;
+                    }else{
+                        check[0] = false;
+                    }
+                }else if(check2[0]){
+                    char tmp;
+                    int count = 0;
+                    for(int i=0; i<editright.getText().toString().length();i++) {
+                        tmp = editright.getText().toString().charAt(i);
+                        int num = (int)tmp;
+                        if(!(num==45 || num==46 || (num>=48 && num<=57))) {
+                            count++;
+                        }
+                    }
+                    if(count!=0){
+                        Toast.makeText(context, "우안 시력을 숫자로 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        check2[0] =true;
+                    }else{
+                        check2[0] = false;
+                    }
                 }else if((Float.valueOf(editleft.getText().toString())>=-20.0 && Float.valueOf(editleft.getText().toString())<=10.0)==false) {
                     Toast.makeText(context, "좌안 시력이 범위를 벗어났습니다.", Toast.LENGTH_SHORT).show();
                 }else if((Float.valueOf(editright.getText().toString())>=-20.0 && Float.valueOf(editright.getText().toString())<=10.0)==false) {
                     Toast.makeText(context, "우안 시력이 범위를 벗어났습니다.", Toast.LENGTH_SHORT).show();
                 }else{
                     db.UserDao().insert(new UserInfo(db.UserDao().getName(),editleft.getText().toString(),editright.getText().toString(),fdate.format(date)));
-                    Toast.makeText(context,"이름이 변경되었습니다.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context,"시력이 변경되었습니다.",Toast.LENGTH_SHORT).show();
                     getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, User).commitAllowingStateLoss();
                 }
             }
