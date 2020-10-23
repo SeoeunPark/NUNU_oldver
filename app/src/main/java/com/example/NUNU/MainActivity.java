dd993cd2577619115738d66f65bc157148d5d72a
seobin Park <seobin0203@naver.com>
2020-10-23 21:21:40 +0900

    #2 스플래시 화면 디자인 변경 및 앱 아이콘 디자인 변경

--- app/src/main/java/com/example/NUNU/MainActivity.java
+++ app/src/main/java/com/example/NUNU/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     default:
                         return false;
-                }
+                 }
             }
         });
     }
