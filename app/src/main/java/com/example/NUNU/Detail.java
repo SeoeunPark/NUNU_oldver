ab350ec73309b2d4f398c8856001f9f8e8ce82da
SeoeunPark <s2019w29@e-mirim.hs.kr>
2020-11-09 01:53:20 +0900

    [수정] Detail.java D-day 기능 추가

--- app/src/main/java/com/example/NUNU/Detail.java
+++ app/src/main/java/com/example/NUNU/Detail.java
@@ -1,13 +1,18 @@
 package com.example.NUNU;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDate;
+import java.util.Calendar;
 
 public class Detail extends AppCompatActivity {
     private static final String TAG= "Detail";
@@ -23,7 +28,17 @@ public class Detail extends AppCompatActivity {
     private TextView dperiod_e;
     private TextView dinfo;
 
+    private String idday;
+    private String idname;
+    private String idtype;
+    private String idcnt;
+    private String idtype_om;
+    private String idperiod_s;
+    private String idperiod_e;
+    private String idinfo;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,16 +57,39 @@ public class Detail extends AppCompatActivity {
         dperiod_e = (TextView)findViewById(R.id.detail_period_end);
         dinfo = (TextView)findViewById(R.id.detail_info);
 
-        dday.setText("3");
-        dname.setText(intent.getExtras().getString("name"));
-        dtype.setText(intent.getExtras().getString("type"));
-        dcnt.setText(Integer.toString(intent.getExtras().getInt("cnt")));
-        dtype_om.setText(Integer.toString(intent.getExtras().getInt("period")));
-                //intent.getExtras().getString("cl");
-        dperiod_s.setText(intent.getExtras().getString("start"));
-        dperiod_e.setText(intent.getExtras().getString("end"));
+        //intent로 값 받아오기
+        idname = intent.getExtras().getString("name");
+        idtype= intent.getExtras().getString("type");
+        idcnt= Integer.toString(intent.getExtras().getInt("cnt"));
+        idtype_om=Integer.toString(intent.getExtras().getInt("period")); //1 이면 원데이 2이면 먼슬리
+        idperiod_s=intent.getExtras().getString("start");
+        idperiod_e=intent.getExtras().getString("end");
+        //디데이 구하기
+        String str = idperiod_e;
+        String[] array = str.split("/");
+        int cyear= Integer.parseInt(array[0]);
+        int cmonth= Integer.parseInt(array[1]);
+        int cday= Integer.parseInt(array[2]);
+
+        LocalDate fromDate = LocalDate.now();
+        LocalDate toDate = LocalDate.of(cyear, cmonth, cday);
+        long substract = ChronoUnit.DAYS.between(fromDate, toDate);
+        //원데이인지 / 먼슬리인지
+        if(idtype_om=="1"){
+            idtype_om ="원데이 렌즈";
+        }else{
+            idtype_om="먼슬리 렌즈";
+        }
 
 
+
+        dday.setText(Integer.toString((int)substract));
+        dname.setText(idname);
+        dtype.setText(idtype);
+        dcnt.setText(idcnt);
+        dtype_om.setText(idtype_om);
+        dperiod_s.setText(idperiod_s);
+        dperiod_e.setText(idperiod_e);
 
         cancel.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -64,9 +102,6 @@ public class Detail extends AppCompatActivity {
                 finish();
             }
         });
-
-
-        //Note word = new Note(intent.getExtras().getString("name"),intent.getExtras().getString("type"),intent.getExtras().getInt("cnt"),intent.getExtras().getInt("period"),intent.getExtras().getString("cl"),intent.getExtras().getString("start"),intent.getExtras().getString("end"));
 
     }
 }
