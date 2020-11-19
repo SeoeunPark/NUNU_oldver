e97e574a0f779675bdda8af59444fc8cba07abab
SeoeunPark <s2019w29@e-mirim.hs.kr>
2020-11-19 19:16:26 +0900

    Home을 렌즈 페이지로 변경

--- app/src/main/java/com/example/NUNU/EyeTest.java
+++ app/src/main/java/com/example/NUNU/EyeTest.java
@@ -9,10 +9,10 @@ import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Home#newInstance} factory method to
+ * Use the {@link EyeTest#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Home extends Fragment {
+public class EyeTest extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +23,7 @@ public class Home extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Home() {
+    public EyeTest() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class Home extends Fragment {
      */
 
     // TODO: Rename and change types and number of parameters
-    public static Home newInstance(String param1, String param2) {
-        Home fragment = new Home();
+    public static EyeTest newInstance(String param1, String param2) {
+        EyeTest fragment = new EyeTest();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_eye_test, container, false);
     }
 }
--- app/src/main/java/com/example/NUNU/MainActivity.java
+++ app/src/main/java/com/example/NUNU/MainActivity.java
@@ -2,21 +2,17 @@ package com.example.NUNU;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.room.Room;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    Home fragment1; // 홈 fragment
-    Lens fragment2; // 렌즈 fragment
+    Lens fragment1; // 렌즈 fragment
+    EyeTest fragment2; // 홈 fragment
     User fragment3; // 유저 fragment
 
     @Override
@@ -25,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         //프래그먼트 생성
-        fragment1 = new Home();
-        fragment2 = new Lens();
+        fragment1 = new Lens();
+        fragment2 = new EyeTest();
         fragment3 = new User();
 
         //제일 처음 보여지는 창
@@ -41,11 +37,11 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     //my_navigation_items.xml에서 지정해줬던 아이디 값을 받아와서 각 아이디값마다 다른 이벤트를 발생
-                    case R.id.home: {
+                    case R.id.eye: {
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment1).commitAllowingStateLoss();
                         return true;
                     }
-                    case R.id.eye: {
+                    case R.id.home: {
                         getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment2).commitAllowingStateLoss();
                         return true;
                     }
