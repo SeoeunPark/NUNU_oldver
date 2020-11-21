becb532c1bc8cf79afeee0be2bd14fa3101ecd56
seobin Park <seobin0203@naver.com>
2020-11-21 21:22:51 +0900

    입력 받은 값 확인

--- app/src/main/java/com/example/NUNU/InitInfo.java
+++ app/src/main/java/com/example/NUNU/InitInfo.java
@@ -41,14 +41,65 @@ public class InitInfo extends AppCompatActivity {
         show_data.setText(db.UserDao().getAll().toString());
 
         add_button.setOnClickListener(new Button.OnClickListener() {
+            public void insertNum(){
+                Boolean check = true;
+                Boolean check2 = true;
+                if(check){
+                    char tmp;
+                    int count = 0;
+                    for(int i=0; i<set_left.getText().toString().length();i++) {
+                        tmp = set_left.getText().toString().charAt(i);
+                        int num = (int)tmp;
+                        if(!(num==45 || num==46 || (num>=48 && num<=57))) {
+                            count++;
+                        }
+                    }
+                    if(count!=0){
+                        Toast.makeText(InitInfo.this, "좌안 시력을 숫자로 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        check=true;
+                    }else{
+                        check = false;
+                    }
+                }else if(check2){
+                    char tmp;
+                    int count = 0;
+                    for(int i=0; i<set_right.getText().toString().length();i++) {
+                        tmp = set_right.getText().toString().charAt(i);
+                        int num = (int)tmp;
+                        if(!(num==45 || num==46 || (num>=48 && num<=57))) {
+                            count++;
+                        }
+                    }
+                    if(count!=0){
+                        Toast.makeText(InitInfo.this, "우안 시력을 숫자로 입력해주세요.", Toast.LENGTH_SHORT).show();
+                        check2=true;
+                    }else{
+                        check2 = false;
+                    }
+                }
+            }
             @Override
             public void onClick(View v) {
-
                 SimpleDateFormat fdate = new SimpleDateFormat("MM-dd");
                 Date date = new Date();
-                db.UserDao().insert(new UserInfo(set_name.getText().toString(), set_left.getText().toString(), set_right.getText().toString(), fdate.format(date)));
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
+                insertNum();
+                if(TextUtils.isEmpty(set_name.getText().toString())){
+                    Toast.makeText(InitInfo.this,"이름을 입력해주세요.",Toast.LENGTH_SHORT).show();
+                }else if(TextUtils.isEmpty(set_left.getText().toString())){
+                    Toast.makeText(InitInfo.this,"좌안 시력을 입력해주세요.",Toast.LENGTH_SHORT).show();
+                }else if(TextUtils.isEmpty(set_right.getText().toString())) {
+                    Toast.makeText(InitInfo.this, "우안 시력을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }else if((Float.valueOf(set_left.getText().toString())>=-20.0 && Float.valueOf(set_left.getText().toString())<=10.0)==false) {
+                    Toast.makeText(InitInfo.this, "좌안 시력이 범위를 벗어났습니다.", Toast.LENGTH_SHORT).show();
+                }else if((Float.valueOf(set_right.getText().toString())>=-20.0 && Float.valueOf(set_right.getText().toString())<=10.0)==false) {
+                    Toast.makeText(InitInfo.this, "우안 시력이 범위를 벗어났습니다.", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(InitInfo.this,"Done",Toast.LENGTH_SHORT).show();
+                    db.UserDao().insert(new UserInfo(set_name.getText().toString(),set_left.getText().toString(),set_right.getText().toString(),fdate.format(date)));
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }
+
             }
 
         });
