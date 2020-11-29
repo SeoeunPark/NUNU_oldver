6ae7312c4421d2fe6fdb1f04fe17a78def42dc37
seobin Park <seobin0203@naver.com>
2020-11-29 20:08:30 +0900

    앱 권한 설정 화면으로 이동

--- app/src/main/java/com/example/NUNU/Option.java
+++ app/src/main/java/com/example/NUNU/Option.java
@@ -6,11 +6,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -44,6 +46,14 @@ public class Option extends Fragment {
             }
         });
 
+        ImageView goSettingbtn = (ImageView)view.findViewById(R.id.goSetting);
+        goSettingbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_SETTINGS);
+                startActivityForResult(intent, 0);
+            }
+        });
         Button changeName = (Button)view.findViewById(R.id.editname_btn);
         changeName.setOnClickListener(new View.OnClickListener(){
             @Override
